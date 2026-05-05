669a4e9e73984a33602ccbe1005269d1698f07eb
Ciprian Tudor <ciprian.tudor@orbyta.it>
2026-05-05 12:19:51 +0200

    set up Express server and update configuration files
    Signed-off-by: Ciprian Tudor <ciprian.tudor@orbyta.it>

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Changelog as a Service API' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
